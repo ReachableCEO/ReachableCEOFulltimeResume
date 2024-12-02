35da87b012e4ba0b932302ebc2e1430f11800862
ReachableCEO <reachableceo@turnsys.com>
2024-12-02 10:05:10 -0600

    preparing long from release

---
+++ cv/build-machine-cv.sh
@@ -0,0 +1,56 @@
+############################################################
+# Machine readable CV for the various employment platforms
+############################################################
+
+#Per platform specific notes....
+# Original idea here was to use the CSV file (| separated but whatever) and figure out (per platform) what was needed for formatting to be
+# auto parsed
+# ie
+
+# function linkedin
+# COMPANY=$1
+# TITLE=$1
+# EMPLOYMENTDATE=$1
+# $COMPANY $EMPLYMENTDATE $TITLE
+
+# function glassdoor
+# COMPANY=$1
+# TITLE=$1
+# EMPLOYMENTDATE=$1
+# $COMPANY $TITLE $EMPLOYMENTDATE
+
+# This may still be developed
+
+# glassdoor
+# Appears to not try to parse.
+
+# indeed
+# Appears to not try to parse.
+
+#  ziprecruiter
+# ZipRecruiter (position parsing) (fixed manually, only one position wasn't properly captured)
+
+#  linkedin
+# TBD, not sure how/if/when it parses the uploaded document...
+
+#  upwork
+# Doesn't seem to parse the resume at all
+
+#  roberthalf
+# Robert Half (not sure if it parses resume or not)
+
+#  dice
+# DIce (skills)
+
+#  teal
+# tbd
+
+#  guru
+# tbd
+
+# careerbuilder
+# tbd
+
+# oracle talent something something (most big companies appear to use this)
+# tbd (once i apply for a job somewhere that uses that platform, i will update)
+

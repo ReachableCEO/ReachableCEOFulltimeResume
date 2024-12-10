0c6d564458d0ab568ad155097adf40b2bd31f427
ReachableCEO <reachableceo@turnsys.com>
2024-12-10 17:34:37 -0600

    rapid iteration. fixing as we go and customizing to my specific details and setup.

--- local/build/build-resume.sh
+++ local/build/build-resume.sh
@@ -94,22 +94,23 @@ export CandidateRateSheetRemote1099HourlyPrefer="\$85.00"
 
 
 export MO_PATH="bash ../../vendor/git.knownelement.com/ExternalVendorCode/mo/mo"
-export BUILD_OUTPUT_DIR="$PipelineClientWorkingDir/build-output"
+
 export BUILD_TEMP_DIR="$PipelineClientWorkingDir/build-temp"
 export BUILDYAML_JOBBOARD="$BUILD_TEMP_DIR/JobBoard.yml"
 export BUILDYAML_CLIENTSUBMISSION="$BUILD_TEMP_DIR/ClientSubmission.yml"
 export BUILDYAML_CANDIDATEINFOSHEET="$BUILD_TEMP_DIR/CandidateInfoSheet.yml"
 
-export CandidateInfoSheetMarkdownOutputFile="$BUILD_OUTPUT_DIR/CandidateInfoSheet.md"
-export CandidateInfoSheetPDFOutputFIle="$BUILD_OUTPUT_DIR/CandidateInfoSheet.pdf"
+export BUILD_OUTPUT_DIR="D:/tsys/@ReachableCEO/resume.reachableceo.com/"
+export CandidateInfoSheetMarkdownOutputFile="$BUILD_OUTPUT_DIR/recruiter/CandidateInfoSheet.md"
+export CandidateInfoSheetPDFOutputFIle="$BUILD_OUTPUT_DIR/recruiter/CandidateInfoSheet.pdf"
 
-export JobBoardMarkdownOutputFile="$BUILD_OUTPUT_DIR/job-board/Resume.md"
-export JobBoardPDFOutputFile="$BUILD_OUTPUT_DIR/job-board/Resume.pdf"
-export JobBoardMSWordOutputFile="$BUILD_OUTPUT_DIR/job-board/Resume.doc"
+export JobBoardMarkdownOutputFile="$BUILD_OUTPUT_DIR/job-board/CharlesNWyble-Resume.md"
+export JobBoardPDFOutputFile="$BUILD_OUTPUT_DIR/job-board/CharlesNWyble-Resume.pdf"
+export JobBoardMSWordOutputFile="$BUILD_OUTPUT_DIR/job-board/CharlesNWyble-Resume.doc"
 
-export ClientSubmissionMarkdownOutputFile="$BUILD_OUTPUT_DIR/client-submission/Resume.md"
-export ClientSubmissionPDFOutputFile="$BUILD_OUTPUT_DIR/client-submission/Resume.pdf"
-export ClientSubmissionMSWordOutputFile="$BUILD_OUTPUT_DIR/client-submission/Resume.doc"
+export ClientSubmissionMarkdownOutputFile="$BUILD_OUTPUT_DIR/client-submit/CharlesNWyble-Resume.md"
+export ClientSubmissionPDFOutputFile="$BUILD_OUTPUT_DIR/client-submit/CharlesNWyble-Resume.pdf"
+export ClientSubmissionMSWordOutputFile="$BUILD_OUTPUT_DIR/client-submit/CharlesNWyble-Resume.doc"
 
 # Call the build-pipeline-server in the vendored repository to produce output artifacts
 

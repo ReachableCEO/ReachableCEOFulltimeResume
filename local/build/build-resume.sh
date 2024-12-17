891928f1b6e42061f3bc6b601b684b6c8ee236fb
Charles N Wyble <reachableceo@turnsys.com>
2024-12-17 11:55:38 -0600

    a few more variables and DRY fixes

--- local/build/build-resume.sh
+++ local/build/build-resume.sh
@@ -10,30 +10,16 @@ source ./CandidateVariables.env
 #DO NOT CHANGE ANYTHING BELOW THIS LINE
 ####################################################
 
+
 ##################################################################
 # Setup globals for use by the build-pipeline-server.sh script
 ##################################################################
 
-
 export MO_PATH="bash ../../vendor/git.knownelement.com/ExternalVendorCode/mo/mo"
-
 export BUILD_TEMP_DIR="$PipelineClientWorkingDir/build-temp/MarkdownResume"
-export BUILD_OUTPUT_DIR="D:/tsys/@ReachableCEO/resume.reachableceo.com"
-
 export BUILDYAML_JOBBOARD="$BUILD_TEMP_DIR/JobBoard.yml"
 export BUILDYAML_CLIENTSUBMISSION="$BUILD_TEMP_DIR/ClientSubmission.yml"
 export BUILDYAML_CANDIDATEINFOSHEET="$BUILD_TEMP_DIR/CandidateInfoSheet.yml"
-
-export CandidateInfoSheetMarkdownOutputFile="$BUILD_OUTPUT_DIR/recruiter/CandidateInfoSheet.md"
-export CandidateInfoSheetPDFOutputFile="$BUILD_OUTPUT_DIR/recruiter/CandidateInfoSheet.pdf"
-
-export JobBoardMarkdownOutputFile="$BUILD_OUTPUT_DIR/job-board/CharlesNWyble-Resume.md"
-export JobBoardPDFOutputFile="$BUILD_OUTPUT_DIR/job-board/CharlesNWyble-Resume.pdf"
-export JobBoardMSWordOutputFile="$BUILD_OUTPUT_DIR/job-board/CharlesNWyble-Resume.doc"
-
-export ClientSubmissionMarkdownOutputFile="$BUILD_OUTPUT_DIR/client-submit/CharlesNWyble-Resume.md"
-export ClientSubmissionPDFOutputFile="$BUILD_OUTPUT_DIR/client-submit/CharlesNWyble-Resume.pdf"
-export ClientSubmissionMSWordOutputFile="$BUILD_OUTPUT_DIR/client-submit/CharlesNWyble-Resume.doc"
 
 # Cleanup previous intermediatge and final output artifacts
 
